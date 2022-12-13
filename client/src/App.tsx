44a77cce79e0ef71893d3b6fb7c02b678a8cac55
leegayeong0114 <56706693+leegayeong0114@users.noreply.github.com>
2022-12-13 22:47:56 +0900

    수정한다!!

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,6 +9,13 @@ function App() {
 
   const [userList, setUserList] = useState<IUser[]>([])
 
+  const [loginEmail, setLoginEmail] = useState('')
+  const [loginPassword, setLoginPassword] = useState('')
+
+  const [signupName, setSignupName] = useState('')
+  const [signupEmail, setSignupEmail] = useState('')
+  const [signupPassword, setSignupPassword] = useState('')
+
   useEffect(() => {
     const getAllUser = async () => {
       try {
@@ -20,6 +27,30 @@ function App() {
     }
     getAllUser()
   }, [])
+
+  const onClickLoginkHandler = async () => {
+    let data = {
+      name: signupName,
+      email: signupEmail,
+      password: signupPassword
+    }
+
+    const res = await axios.post('/api/users/login', data)
+    console.log(res.data)
+    if(!res.data.success) alert(res.data.errorMsg)
+  }
+
+  const onClickSignupkHandler = async () => {
+    let data = {
+      name: signupName,
+      email: signupEmail,
+      password: signupPassword
+    }
+
+    const res = await axios.post('/api/users/signup', data)
+    console.log(res.data)
+    if(!res.data.success) alert(res.data.errorMsg)
+  }
   
   return (
     <div className='app'>
@@ -28,6 +59,19 @@ function App() {
           return <h5 key={idx}>{idx + 1}. {user.email} / {user.password}</h5>
         })
       }
+      <div>
+        <p>로그인</p>
+        <input type="text" placeholder="email" value={loginEmail} onChange={(e) => setLoginEmail(e.target.value)}/><br/>
+        <input type="text" placeholder="password" value={loginPassword} onChange={(e) => setLoginPassword(e.target.value)}/><br/>
+        <button type="button" onClick={onClickLoginkHandler}>로그인</button>
+      </div>
+      <div>
+        <p>회원가입</p>
+        <input type="text" placeholder="name" value={signupName} onChange={(e) => setSignupName(e.target.value)}/><br/>
+        <input type="text" placeholder="email" value={signupEmail} onChange={(e) => setSignupEmail(e.target.value)}/><br/>
+        <input type="text" placeholder="password" value={signupPassword} onChange={(e) => setSignupPassword(e.target.value)}/><br/>
+        <button type="button" onClick={onClickSignupkHandler}>회원가입</button>
+      </div>
     </div>
   )
 }
